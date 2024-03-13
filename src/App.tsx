9690c755b30f0a7f087715c9821e80a1855fa9a9
syf <2654525303@qq.com>
2024-03-14 02:00:08 +0800

    test print ability

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import './App.css'
-import { Box, Center } from "@chakra-ui/react";
+import { Box, Button, Center } from "@chakra-ui/react";
 import { CV } from "./cv/CV.tsx";
 import { AppBar } from "./layout/AppBar.tsx";
+import { useRef } from "react";
+import { useReactToPrint } from "react-to-print";
 
 function App() {
+
+    const cvRef = useRef(null)
+    const handlePrint = useReactToPrint({
+        content: () => cvRef.current
+    })
 
     return (
         <>
             <AppBar/>
+
+            <Button colorScheme={'blue'} onClick={handlePrint}>Print</Button>
             <Center mt={'5rem'}>
                 <Box width={'210mm'} height={'297mm'} border={'2px dashed'}>
-                    <CV/>
+                    <CV ref={cvRef}/>
                 </Box>
             </Center>
         </>
--- src/cv/CV.tsx
+++ src/cv/CV.tsx
@@ -1,7 +1,8 @@
 import { TitleBar } from "./components/TitleBar.tsx";
+import { forwardRef } from "react";
 
-export function CV() {
-    return <>
+export const CV = forwardRef<HTMLDivElement>((_, ref) => {
+    return <div ref={ref}>
         <TitleBar/>
-    </>
-}
+    </div>
+})
